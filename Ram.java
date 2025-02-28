4b8a9ba2414ba009410a7493bdf25aec37182ded
DavidRamsec <davidramswezz@gmail.com>
2025-02-28 10:09:07 +0100

    Commit 1

---
+++ Ram.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Memoria here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ram
+{
+    // instance variables - replace the example below with your own
+    private int memoria;
+    private String formato;
+
+    /**
+     * Constructor for objects of class Memoria
+     */
+    public Ram()
+    {
+        memoria = 8000;
+        formato = "ddr5";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void setMemoria(int memoria){
+        this.memoria = memoria;
+    }
+    
+    public void setFormato(String formato){
+        this.formato = formato;
+    }
+}
